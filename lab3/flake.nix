3a257eaac5c9708edf319d631ed8c38c5183d4ea
Roverp <yar.zubaha@proton.me>
2025-10-29 09:59:51 +0100

    feat(lab3): initial commit

---
+++ lab3/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Environment for lab_1";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [go];
+        };
+
+        shellHook = ''
+          echo "Go environment loaded"
+        '';
+      }
+    );
+}
